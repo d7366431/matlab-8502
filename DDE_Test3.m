bf8bc6066b59134dc3cfff03b0a0e961651445f5
d7366431 <danil_maximov@list.ru>
2018-12-27 10:23:49 +0100

    working solution for xdot after coordinate transformation

--- DDE_Test3.m
+++ DDE_Test3.m
@@ -3,29 +3,21 @@ clear all
 IC = zeros(2*nc.n,1);
 tspan = 1:0.01:40;                %Simulation time
 
-%%sol   = dde23(    ddefile,    lags,        history,           tspan);
-  sol   = dde23(       @dde,    [0.01],        @history,           tspan);
-
-[t,x_nd] = ode45(@dde_nd,tspan,IC);
+%%sol    = dde23(    ddefile,    lags,        history,           tspan);
+  sol    = dde23(       @dde,    [0.01],        @history,           tspan);
+[t,x_nd] = ode45(    @dde_nd,     tspan,      IC);
 
 %% Plots of results  
 ax1 = subplot(3,1,1);
-        plot(sol.x,sol.y);
-    grid on;
-    title('sol.x, sol.y')
-    xlabel('Time t');    ylabel('x(t)');
- 
+    plot(sol.x,sol.y);
+    title('sol.x, sol.y'); grid on; xlabel('Time t');    ylabel('x(t)');   
 ax2 = subplot(3,1,2);
         xplot = 1:0.01:40;
         yplot = deval(sol(1:size(nc.n)),xplot,2*nc.n);
-    grid on;
-    title('deval')
-    plot(xplot,yplot);
-    xlabel('Time t');    ylabel('x(t)');
-
+    title('deval');
+    plot(xplot,yplot);   grid on; xlabel('Time t');    ylabel('x(t)');
 ax2 = subplot(3,1,3);
-    plot(t,x_nd(:,end)); 
-    xlabel('Time t no delay'); ylabel('x(t) no delay');   
+    plot(t,x_nd(:,end)); grid on; xlabel('Time t no delay'); ylabel('x(t) no delay');  
     
 function xdot=dde(t,x,Z)                   % x - state of system (position of each block)
 %% INPUT for DRILLSTRING    
@@ -41,12 +33,18 @@ f1=nc.v0*t;                                % external force f1=f(t)*M^-1 - 9.81
 F1=zeros(nc.n,1);
 F1(1)=f1;
 MVF1=inv(nc.M)*inv(V)*F1;
+%MF1=inv(nc.M)*F1;
 %% D
 D=D*eye(nc.n);
+%VDV=V*D*inv(V);
 %% MExlag                               Energy for cutting - Wc only for last element of drillstring (n)
 E=zeros(nc.n,nc.n);
 E(end,end)=nc.e;                           %last element of vector E is cutting constant e                                       
 ME=inv(nc.M)*E;
+
+% E=zeros(nc.n,1);
+% E(end,1)=nc.e;                           %last element of vector E is cutting constant e                                       
+% ME=inv(nc.M)*E;
 %% The differential equations:
         xlag    = Z(1:nc.n,1);
         x1      = zeros(nc.n,1);
@@ -58,7 +56,8 @@ ME=inv(nc.M)*E;
         x2 = xdot(nc.n+1:end);
         x1dot = x2;
         x2dot = MVF1 - nc.C*x2 -D*x1 + ME*xlag;
-        xdot = [x1dot; x2dot]
+        %x2dot = MF1  - 9.81 -D*x1 - nc.C*x2;
+        xdot = [x1dot; x2dot];
         
 end
 
@@ -76,9 +75,15 @@ f1=nc.v0*t;                                % external force f1=f(t)*M^-1 - 9.81
 F1=zeros(nc.n,1);
 F1(1)=f1;
 MVF1=inv(nc.M)*inv(V)*F1;
+%MF1=inv(nc.M)*F1;
 
 %% D
 D=D*eye(nc.n);
+%VDV=V*D*inv(V);
+%% MExlag                               Energy for cutting - Wc only for last element of drillstring (n)
+E=zeros(nc.n,nc.n);
+E(end,end)=nc.e;                           %last element of vector E is cutting constant e                                       
+ME=inv(nc.M)*E;
 %% The differential equations:
     x1_nd      = zeros(nc.n,1);
     x2_nd      = zeros(nc.n,1);
@@ -86,10 +91,12 @@ D=D*eye(nc.n);
     x2dot_nd   = zeros(nc.n,1);
     xdot_nd    = zeros(2*nc.n,1);
     x1_nd = x_nd(1:nc.n);
-    x2_nd = x_nd(nc.n+1:end);
+    x2_nd = x_nd(nc.n+1:2*nc.n);
     x1dot_nd = x2_nd;
     x2dot_nd = MVF1-nc.C*x2_nd - D*x1_nd; %Lag is zero
+    %x2dot_nd = MF1 +ME*x1_nd - 9.81 -VDV*x1_nd - nc.C*x2_nd; %Lag is zero
     xdot_nd = [x1dot_nd; x2dot_nd];
+
 end
 
 function s = history(t)
