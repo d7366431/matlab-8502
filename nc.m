597f33e03f088114bc78e68afe75103941c164a7
d7366431 <danil_maximov@list.ru>
2018-12-27 10:30:19 +0100

    parameters for 12 27 2018

--- nc.m
+++ nc.m
@@ -1,24 +1,26 @@
 classdef nc
    properties (Constant)
-        n=3;                                       %number of elements for section #3 - length 1350m
-        H=1350;
-        h = nc.H/nc.n;                              %length of each segment
-        ad = 0.0034;                                %area of drilling collar [m^2]
-        v0=10;                                     %constant speed of drilling
-        %% INPUT for BIT         
-        zb=5;                                       %Number of blades
-        a=0.4445/2;                                 %Radius of section#3 in [m]
-                                                    %Orientation of cutting face =1 at 1st iteration
-        se=32e6;                                    %intrinsic specific energy (typically 32 MPa)
-        e=nc.zb*nc.a*nc.se;                         %Cutting Energy       
-        %% INPUT for DRILLSTRING    
-        m=71000/nc.n;                               %mass of each block
+        
+        n=30;                                        % number of elements for section #3 - length 1350m
         E = 200e9;                                  % Young's modulus steel
-        k = nc.E*nc.ad/nc.h;                        % sprint constant (linear)
-        c=535/nc.n;                                 %damping of each block
+        H=1350;
+        h = nc.H/nc.n;                              % length of each segment
+        ad = 0.0034;                                % area of drilling collar [m^2]
+        v0=10;                                      % constant speed of drilling
+        %% INPUT for BIT         
+        zb=5;                                       % Number of blades
+        a=0.4445/2;                                 % Radius of section#3 in [m]
+                                                    % Orientation of cutting face =1 at 1st iteration
+        se=32e6;                                    % intrinsic specific energy (typically 32 MPa)
+        e=nc.zb*nc.a*nc.se;                         % Cutting Energy       
+        %% INPUT for DRILLSTRING    
+        m=71000/nc.n;                               % mass of each block
+        k = nc.n*524287                             % spring constant
+        %k = nc.E*nc.ad/nc.h; 
+        c=535/nc.n;                                 % damping of each block
         %% Matrix
         %M-Mass matrix
-        M=nc.m*eye(nc.n);                           %Matrix of drillstring mass
+        M=nc.m*eye(nc.n);                           % Matrix of drillstring mass
         %C-damping matrix
         C=nc.c*eye(nc.n);
    end
