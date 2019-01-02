602a480f470550a2ca76a9870dc54975e56f3d9a
d7366431 <danil_maximov@list.ru>
2019-01-02 15:40:11 +0100

    Change System of Matrices, verified parameters in .nc, Got results for tspan 1:0.01:4

--- DDE_Test3.m
+++ DDE_Test3.m
@@ -1,89 +1,93 @@
 clc
 clear all
 IC = zeros(2*nc.n,1);
-tspan = 1:0.01:40;                %Simulation time
+tspan = 1:0.01:4;                %Simulation time
 
-%%sol    = dde23(    ddefile,    lags,        history,           tspan);
-  sol    = dde23(       @dde,    [0.01],        @history,           tspan);
-[t,x_nd] = ode45(    @dde_nd,     tspan,      IC);
+    %sol    = dde23(    ddefile,    lags,        history,           tspan);
+     sol      = dde23(       @dde,    [0.01],      @history,          tspan);
+    [t,x_nd] = ode45(    @dde_nd,     tspan,      IC);
 
 %% Plots of results  
 ax1 = subplot(3,1,1);
     plot(sol.x,sol.y);
     title('sol.x, sol.y'); grid on; xlabel('Time t');    ylabel('x(t)');   
 ax2 = subplot(3,1,2);
-        xplot = 1:0.01:40;
+        xplot = 1:0.01:4;
         yplot = deval(sol(1:size(nc.n)),xplot,2*nc.n);
     title('deval');
     plot(xplot,yplot);   grid on; xlabel('Time t');    ylabel('x(t)');
 ax2 = subplot(3,1,3);
     plot(t,x_nd(:,end)); grid on; xlabel('Time t no delay'); ylabel('x(t) no delay');  
     
-function xdot=dde(t,x,Z)                   % x - state of system (position of each block)
+
+function xdot=dde(t,x,Z)                    % x - state of system (position of each block)
 %% INPUT for DRILLSTRING    
-%Stiffness matrix
-K=full(gallery('tridiag',nc.n,-1,2,-1));   %spring
-K(end,end)=1;
-K=nc.k*K;
-K(end,end)=K(end,end)+nc.e;                %put last right bottom value as sum of spring (K) and "Cutting constant" (e)
+% Stiffness matrix
+%put last right bottom value as sum of spring (K) and "Cutting constant" (e)
+K=full(gallery('tridiag',nc.n,-1,2,-1));    %spring
+K(end,end)=1;                               %put last right bottom value to 1
+K=nc.k*K;                                   %Stiffness matrix
+K(end,end)=K(end,end)+nc.e;
 %Eigenvalue decomposition
-[V,D] = eig(K,nc.M);    
-%% MVF1      
-f1=nc.v0*t;                                % external force f1=f(t)*M^-1 - 9.81
-F1=zeros(nc.n,1);
-F1(1)=f1;
-MVF1=inv(nc.M)*inv(V)*F1;
-%MF1=inv(nc.M)*F1;
+[V,D] = eig(inv(nc.M),K);    
+%[V,D] = eig(K,nc.M); 
+%% MVv0t                                 
+v0t=nc.v0*t;                                 % external force f1=f(t)*M^-1 - 9.81
+MVv0t=zeros(nc.n,1);
+MVv0t(1)=v0t;
+MVv0t=inv(nc.M*V)*MVv0t;
+%% MVE without lag                          Energy for cutting - Wc only for last element of drillstring (n)
+E=zeros(nc.n,1);
+E(end,end)=nc.e;                            %last element of vector E is cutting constant e                                       
+MVE=inv(nc.M*V)*E;
+%% VgB
+gB=9.81*nc.B;
+GB=gB*eye(nc.n);
+VGB=inv(V)*GB;
 %% D
 D=D*eye(nc.n);
 %VDV=V*D*inv(V);
-%% MExlag                               Energy for cutting - Wc only for last element of drillstring (n)
-E=zeros(nc.n,nc.n);
-E(end,end)=nc.e;                           %last element of vector E is cutting constant e                                       
-ME=inv(nc.M)*E;
-
-% E=zeros(nc.n,1);
-% E(end,1)=nc.e;                           %last element of vector E is cutting constant e                                       
-% ME=inv(nc.M)*E;
 %% The differential equations:
-        xlag    = Z(1:nc.n,1);
-        x1      = zeros(nc.n,1);
-        x2      = zeros(nc.n,1);
-        x1dot   = zeros(nc.n,1);
-        x2dot   = zeros(nc.n,1);
-        xdot    = zeros(2*nc.n,1);
-        x1 = xdot(1:nc.n);
-        x2 = xdot(nc.n+1:end);
-        x1dot = x2;
-        x2dot = MVF1 - nc.C*x2 -D*x1 + ME*xlag;
-        %x2dot = MF1  - 9.81 -D*x1 - nc.C*x2;
-        xdot = [x1dot; x2dot];
-        
+xlag    = Z(1:nc.n,1);    
+x1      = zeros(nc.n,1);
+x2      = zeros(nc.n,1);
+x1dot   = zeros(nc.n,1);
+x2dot   = zeros(nc.n,1);
+xdot    = zeros(2*nc.n,1);
+x1 = x(1:nc.n);
+x2 = x(nc.n+1:2*nc.n);
+x1dot = x2;
+x2dot = MVv0t + MVE.*xlag - D*x1 -  nc.C*x2;                       %Lag is zero %%Missing VGB - GETTING ERROR
+xdot = [x1dot; x2dot];
 end
 
-function xdot_nd=dde_nd(t,x_nd)                % x - state of system (position of each block)
+function xdot_nd=dde_nd(t,x_nd)             % x - state of system (position of each block)
 %% INPUT for DRILLSTRING
-%Stiffness matrix
-              %put last right bottom value as sum of spring (K) and "Cutting constant" (e)
-K=full(gallery('tridiag',nc.n,-1,2,-1));   %spring
-K=nc.k*K;
-K(end,end)=nc.k+nc.e;
+% Stiffness matrix
+%put last right bottom value as sum of spring (K) and "Cutting constant" (e)
+K=full(gallery('tridiag',nc.n,-1,2,-1));    %spring
+K(end,end)=1;                               %put last right bottom value to 1
+K=nc.k*K;                                   %Stiffness matrix
+K(end,end)=K(end,end)+nc.e;
 %Eigenvalue decomposition
-[V,D] = eig(K,nc.M);    
-%% MVF1                                 
-f1=nc.v0*t;                                % external force f1=f(t)*M^-1 - 9.81
-F1=zeros(nc.n,1);
-F1(1)=f1;
-MVF1=inv(nc.M)*inv(V)*F1;
-%MF1=inv(nc.M)*F1;
-
+[V,D] = eig(inv(nc.M),K);    
+%[V,D] = eig(K,nc.M); 
+%% MVv0t                                 
+v0t=nc.v0*t;                                 % external force f1=f(t)*M^-1 - 9.81
+MVv0t=zeros(nc.n,1);
+MVv0t(1)=v0t;
+MVv0t=inv(nc.M*V)*MVv0t;
+%% MVE without lag                          Energy for cutting - Wc only for last element of drillstring (n)
+E=zeros(nc.n,1);
+E(end,end)=nc.e;                            %last element of vector E is cutting constant e                                       
+MVE=inv(nc.M*V)*E;
+%% VgB
+gB=9.81*nc.B;
+GB=gB*eye(nc.n);
+VGB=inv(V)*GB;
 %% D
 D=D*eye(nc.n);
 %VDV=V*D*inv(V);
-%% MExlag                               Energy for cutting - Wc only for last element of drillstring (n)
-E=zeros(nc.n,nc.n);
-E(end,end)=nc.e;                           %last element of vector E is cutting constant e                                       
-ME=inv(nc.M)*E;
 %% The differential equations:
     x1_nd      = zeros(nc.n,1);
     x2_nd      = zeros(nc.n,1);
@@ -93,10 +97,8 @@ ME=inv(nc.M)*E;
     x1_nd = x_nd(1:nc.n);
     x2_nd = x_nd(nc.n+1:2*nc.n);
     x1dot_nd = x2_nd;
-    x2dot_nd = MVF1-nc.C*x2_nd - D*x1_nd; %Lag is zero
-    %x2dot_nd = MF1 +ME*x1_nd - 9.81 -VDV*x1_nd - nc.C*x2_nd; %Lag is zero
+    x2dot_nd = MVv0t + MVE.*x1_nd - D*x1_nd -  nc.C*x2_nd;                       %Lag is zero %%Missing VGB - GETTING ERROR
     xdot_nd = [x1dot_nd; x2dot_nd];
-
 end
 
 function s = history(t)
--- nc.m
+++ nc.m
@@ -1,12 +1,11 @@
 classdef nc
-   properties (Constant)
-        
-        n=30;                                        % number of elements for section #3 - length 1350m
+   properties (Constant) 
+        n=3;                                        % number of elements for section #3 - length 1350m
         E = 200e9;                                  % Young's modulus steel
         H=1350;
         h = nc.H/nc.n;                              % length of each segment
-        ad = 0.0034;                                % area of drilling collar [m^2]
-        v0=10;                                      % constant speed of drilling
+        v0=100;                                      % constant speed of drilling
+        B=0.825;                                    % bouyancy factor
         %% INPUT for BIT         
         zb=5;                                       % Number of blades
         a=0.4445/2;                                 % Radius of section#3 in [m]
@@ -14,10 +13,9 @@ classdef nc
         se=32e6;                                    % intrinsic specific energy (typically 32 MPa)
         e=nc.zb*nc.a*nc.se;                         % Cutting Energy       
         %% INPUT for DRILLSTRING    
-        m=71000/nc.n;                               % mass of each block
-        k = nc.n*524287                             % spring constant
-        %k = nc.E*nc.ad/nc.h; 
-        c=535/nc.n;                                 % damping of each block
+        m=71000/nc.n;                               % mass of each block        M*/n
+        k = nc.n*524287                             % spring constant           n*K
+        c=535/nc.n;                                 % damping of each block     C/n
         %% Matrix
         %M-Mass matrix
         M=nc.m*eye(nc.n);                           % Matrix of drillstring mass
